5ed9a64715b9675735be4af0703e15636c31369b
Adrian Widłak <adrian.widlak@interia.pl>
2026-02-18 13:45:28 +0100

    Nav

---
+++ src/components/nav/nav.js
@@ -0,0 +1,18 @@
+import './nav.css';
+
+export function Nav() {
+
+
+    return (
+        <nav>
+            <span>LOGO</span>
+
+            <ul>
+                <li>DASHBOARD</li>
+                <li>NEWS</li>
+                <li>USERS</li>
+            </ul>
+        </nav>
+    )
+
+}
