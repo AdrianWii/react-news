81ad85332ae4c0cb063c7c40ca535b7af08355d4
Adrian Widłak <adrian.widlak@interia.pl>
2026-02-18 12:02:58 +0100

    Styl dla tekstu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Adrian";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="animated-title">HELLO {name}!</h1>
+        </header>
+      </div>
+      <div></div>
+    </>
   );
 }
 
